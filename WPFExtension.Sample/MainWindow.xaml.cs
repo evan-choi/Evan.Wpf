14c7f6dc7a0a5549d1cac74c7b84b577c79c7333
Steal <choejin-yong@B43C>
2017-02-05 02:05:40 +0900

    초기 커밋.

---
+++ WPFExtension.Sample/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using WPFExtension;
+using System.Windows;
+using System;
+
+namespace WPFExtension.Sample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var model = new Model()
+            {
+                Title = "Hello Depedency Helper!"
+            };
+
+            this.DataContext = model;
+
+            Model.TitleProperty.AddValueChanged(model, TitlePropertyChanged);
+        }
+
+        private void TitlePropertyChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Changed");
+        }
+    }
+}
---
+++ WPFExtension.Sample/Model.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFExtension.Sample
+{
+    public class Model : DependencyObject
+    {
+        public static readonly DependencyProperty TitleProperty =
+            DependencyHelper.Register();
+
+        private static void TitleChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+    }
+}
