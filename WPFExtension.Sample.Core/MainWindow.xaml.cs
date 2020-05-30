36bf0439fa145402b49998c2c5aee6be5bba7a94
dtaylor-530 <d.taylor530@gmail.com>
2020-05-30 20:11:33 +0100

    Converted WPFExtension project into new csproj format. Targeted .Net 4.5 and .NetCore 3.1. Added new Sample project targeting .NetCore 3.1

---
+++ WPFExtension.Sample.Core/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using WPFExtension;
+using System.Windows;
+using System;
+
+namespace WPFExtension.Sample
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var model = new Model()
+            {
+                Title = "Hello Depedency Helper!"
+            };
+
+            this.DataContext = model;
+
+            Model.TitleProperty.AddValueChanged(model, TitlePropertyChanged);
+
+            this.Text = "A piece of text";
+        }
+
+
+        public static DependencyProperty TextProperty = DependencyHelper.Register();
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        private void TitlePropertyChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("Changed");
+        }
+    }
+}
---
+++ WPFExtension.Sample.Core/Model.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+
+namespace WPFExtension.Sample
+{
+    public class Model : DependencyObject
+    {
+        public static readonly DependencyProperty TitleProperty =
+            DependencyHelper.Register();
+       
+        public string Title
+        {
+            get { return this.GetValue<string>(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+    }
+}
